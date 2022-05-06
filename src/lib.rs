6f7eb400c77cf8f0666eabaead8b7af468a4a98d
Johannes Schilling <dario@deaktualisierung.org>
2022-05-06 22:17:25 +0200

    Apply or-patterns clippy suggestion
    The corresponding feature has been stabilized in rust-lang/rust#79278,
    merged 2021-03-22.

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,6 @@
 #![allow(clippy::similar_names)]
 #![allow(clippy::single_match_else)]
 #![allow(clippy::too_many_lines)]
-// Needs a nightly feature, doesn't bring that much to the table
-// FIXME: Apply Clippy lint once or-patterns are stabilized (rust-lang/rust#54883)
-#![allow(clippy::unnested_or_patterns)]
 #![deny(warnings)]
 
 extern crate rustc_const_eval; // Requires `rustup component add rustc-dev`
